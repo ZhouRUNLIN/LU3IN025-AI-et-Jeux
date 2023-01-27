06f9fb8320dcacdb5663c4d3143ce8ec566f3aa5
mprTest1 <98311505+mprTest1@users.noreply.github.com>
2023-01-27 12:18:18 +0100

    Add files via upload

--- TME1-4/main2.py
+++ TME1-4/main2.py
@@ -2,6 +2,9 @@ import partie1
 import partie2
 
 # Partie 2
-#print(partie2.generate_pref_etu(11))
-#print(partie2.generate_pref_spe(11))
+print(partie2.generate_pref_etu(11))
+print(partie2.generate_pref_spe(11))
 print(partie2.analyse_running_time_HA(9))
+print(partie2.analyse_running_time_HAHO(9))
+partie2.analyse_running_time_graph(200,2000,200)
+# Q7: complexity = O(n2³-n1³)
--- TME1-4/partie2.py
+++ TME1-4/partie2.py
@@ -1,6 +1,7 @@
 import partie1
 import random
 import time
+import matplotlib.pyplot as plt
 
 def generate_pref_etu(n:int):
     pref_etu = []
@@ -23,12 +24,35 @@ def generate_pref_spe(n:int):
 def analyse_running_time_HA(n:int):
     pref_etu = generate_pref_etu(n)
     pref_spe = generate_pref_spe(n)
-    print(pref_etu)
-    print(pref_spe)
     time_passed = time.time()
     assignment = partie1.hospital_algorithm(pref_etu,pref_spe)
     time_passed = time.time() - time_passed
-    print(assignment)
-    assert partie1.stability_verification(assignment,pref_etu,pref_spe)
     return time_passed
 
+def analyse_running_time_HAHO(n:int):
+    pref_etu = generate_pref_etu(n)
+    pref_spe = generate_pref_spe(n)
+    time_passed = time.time()
+    assignment = partie1.hospital_algorithm_Hoptimized(pref_etu,pref_spe)
+    time_passed = time.time() - time_passed
+    return time_passed
+
+def analyse_running_time_graph(n1:int,n2:int,step:int):
+    y1 = []
+    y2 = []
+    for n_value in range(n1,n2+1,step):
+        y1_temp=0
+        y2_temp=0
+        for index_n in range(10):
+            y1_temp += analyse_running_time_HA(n_value)/10.0
+            y2_temp += analyse_running_time_HA(n_value)/10.0
+        y1.append(y1_temp)
+        y2.append(y2_temp)
+    x = [i for i in range(n1,n2+1,step)]
+    l1 = plt.plot(x,y1,'r--',label="Patient-optimized")
+    l2 = plt.plot(x,y2,'b--',label="Hospital-optimized")
+    plt.xlabel('n')
+    plt.ylabel('running time')
+    plt.legend()
+    plt.show()
+    return None
